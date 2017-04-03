43d021d3e7c08b5c0f0ab1d27245a328624b21af
Simon Vadée <simon.vadee@gmail.com>
2017-04-03 14:18:02 +0200

    airbnb maps is working on android

--- index.android.js
+++ index.android.js
@@ -1,42 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Button,
+  StyleSheet
+} from 'react-native'
 
-export default class TalkingStatuesApp extends Component {
+import MapView from 'react-native-maps';
+
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <MapView
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          style={styles.map}
+          onRegionChange={() => {}}
+          onRegionChangeComplete={() => {}}
+          showsUserLocation={true}
+          >
+        </MapView>
       </View>
     );
   }
 }
 
+
+// Class name is name used for StackNavigator navigation
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    // Nav options can be defined as a function of the navigation prop:
+    title: ({ state }) => `${state.params.name}`,
+  };
+  render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Hello, the title comes from params from homepage</Text>
+      </View>
+    );
+  }
+}
+// Remember to add new screens here, match name with class.
+const TalkingStatuesApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
   welcome: {
     fontSize: 20,
