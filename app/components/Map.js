ecf9bc6610f28b87dab5b154f656a0250482b592
André Eriksen <neskire@outlook.com>
2017-05-21 21:44:34 +0200

    Fixed map-pins not printing info from server. changed app name to Statuen, Android side will need be changed in android studio.
    or run
    react-native eject (delete ios and android folder first)
    react-native link
    
    Run like normal, be aware that project settings made within android studio will be lost.

--- app/components/Map.js
+++ app/components/Map.js
@@ -28,7 +28,6 @@ styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom:6,
 
-
   },
 });
 
@@ -54,32 +53,32 @@ export default class Map extends Component {
     };
 
     fetch('http://api.talkingstatues.xyz/statues')
-    .then((response) => response.json())
-    .then((data) => {
-      this.setState({"statues": JSON.parse(data.latest_statue_list)});
-      this.setState({markers_loaded: true});
-    })
-    .catch((error) => console.log(error));
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({"statues": JSON.parse(data.latest_statue_list)});
+          this.setState({markers_loaded: true});
+        })
+        .catch((error) => console.log(error));
 
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        if (pos.coords.latitude && pos.coords.longitude) {
-          this.setState({
-            region : {
-              latitude: pos.coords.latitude,
-              longitude: pos.coords.longitude,
-              latitudeDelta: LATITUDE_DELTA_DEFAULT,
-              longitudeDelta: LONGITUDE_DELTA_DEFAULT,
-            }
-          });
-        }
-      },
-      (error) => alert(JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-      );
+        (pos) => {
+          if (pos.coords.latitude && pos.coords.longitude) {
+            this.setState({
+              region : {
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                latitudeDelta: LATITUDE_DELTA_DEFAULT,
+                longitudeDelta: LONGITUDE_DELTA_DEFAULT,
+              }
+            });
+          }
+        },
+        (error) => alert(JSON.stringify(error)),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
     this.watchID = navigator.geolocation.watchPosition((pos) => {
       if (pos.coords.latitude && pos.coords.longitude) {
         this.setState({
@@ -99,6 +98,7 @@ export default class Map extends Component {
   }
 
   focus_marker(statue_index) {
+    console.info('Focusing on marker: ', statue_index);
     var statue = this.state.statues[statue_index];
     this.setState({
       region : {
@@ -111,85 +111,88 @@ export default class Map extends Component {
   }
 
   display_markers() {
+
     return this.state.statues.map((marker, index) => (
-      <MapView.Marker
-      key={index}
-      coordinate={ marker.fields }
-      title={marker.fields.name}
-      description={marker.fields.description}
-      onPress={() => {
-        if (Platform.OS === 'ios') {
-          this.focus_marker(index)
-        }
-      }}
-      >
-      <MapView.Callout
-      tooltip={true}
-      onPress={() =>
-        {this.props.navigator(1)}
-      }>
-      <CustomCallout>
-      <Text style={styles.header}>{marker.fields.title}</Text>
-      <Image
-      style={{resizeMode: 'contain', height:100}}
-      source={{uri: marker.fields.pictures}}
-      />
-      </CustomCallout>
-      </MapView.Callout>
-      </MapView.Marker>
-      ))
+        <MapView.Marker
+            key={index}
+            coordinate={ marker.fields }
+            title={marker.fields.name}
+            description={marker.fields.description}
+            onPress={() => {
+              if (Platform.OS === 'ios') {
+                this.focus_marker(index)
+              }
+            }}
+        >
+          <MapView.Callout
+              tooltip={true}
+              onPress={() =>
+              {this.props.navigator(1)}
+              }>
+            <CustomCallout>
+              <View style={{}}>
+                <Text style={styles.header}>{marker.fields.name}</Text>
+                <Image
+                    style={{resizeMode: 'contain', height:100}}
+                    source={{uri: marker.fields.pictures}}
+                />
+              </View>
+            </CustomCallout>
+          </MapView.Callout>
+        </MapView.Marker>
+    ))
   }
 
   render_map = (route, navigator) => {
     if (!this.state.markers_loaded) {
       return (
-        <MapView.Animated
-        ref='map'
-        region={this.state.region}
-        style={styles.map}
-        onRegionChange={(region) => {
-          this.setState({region})
-        }}
-        onRegionChangeComplete={() => {}}
-        showsUserLocation={true}
-        >
-        </MapView.Animated>
-        );
+          <MapView.Animated
+              ref='map'
+              region={this.state.region}
+              style={styles.map}
+              onRegionChange={(region) => {
+                this.setState({region})
+              }}
+              onRegionChangeComplete={() => {}}
+              showsUserLocation={true}
+          >
+          </MapView.Animated>
+      );
     } else {
       return (
-        <MapView.Animated
-        ref='map'
-        region={this.state.region}
-        style={styles.map}
-        onRegionChange={(region) => {
-          this.setState({region})
-        }}
-        onRegionChangeComplete={() => {}}
-        showsUserLocation={true}
-        >
-        {  this.display_markers() }
-        </MapView.Animated>
-        );
+          <MapView.Animated
+              ref='map'
+              region={this.state.region}
+              style={styles.map}
+              onRegionChange={(region) => {
+                this.setState({region})
+              }}
+              onRegionChangeComplete={() => {}}
+              showsUserLocation={true}
+          >
+            {  this.display_markers() }
+          </MapView.Animated>
+      );
     }
   }
 // Class is currently unused, semi prepared 
-render(){
-  return(
-    <Navigator
-    navigationBar={
-      <Navigator.NavigationBar
-      routeMapper={{
-        LeftButton: (route, navigator, index, navState) => {},
-        RightButton: (route, navigator, index, navState) =>
-        {},
-        Title: (route, navigator, index, navState) =>
-        {},
-      }}
-      style={{backgroundColor: 'rgba(0, 0, 0, 0.0)'}}
-      />
-    }
-    renderScene={this.render_map}
-    />
+  render(){
+    return(
+        <Navigator
+            navigationBar={
+              <Navigator.NavigationBar
+                  routeMapper={{
+                    LeftButton: (route, navigator, index, navState) => {},
+                    RightButton: (route, navigator, index, navState) =>
+                    {},
+                    Title: (route, navigator, index, navState) =>
+                    {},
+                  }}
+                  style={{backgroundColor: 'rgba(0, 0, 0, 0.0)'}}
+              />
+            }
+            renderScene={this.render_map}
+        />
     );
-}
+  }
 }
