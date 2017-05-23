41cbd113596a719dcb4b58b908061e1d977d3b76
Simon Vadée <simon.vadee@gmail.com>
2017-05-23 10:50:26 +0200

    notification triggered when beacond etected

--- app/components/Beacons.android.js
+++ app/components/Beacons.android.js
@@ -10,12 +10,16 @@ import PushNotification from 'react-native-push-notification'
 
 export default class AbstractBeacon {
 
+	static all_beacons = null;
+	static active_beacons = [];
 
-	isBeaconKnown(beacon_uuid) {
-		for (let i = 0; i < this.beacons.lenght; i++) {
-			if (this.beacons[i].uuid == beacon_uuid)
-				console.log("beacon ".concat(beacon_uuid).concat(" is known"));
-			return true;
+	static isBeaconKnown(beacon_uuid) {
+		if (AbstractBeacon.all_beacons != null) {
+			for (let i = 0; i < AbstractBeacon.all_beacons.length; i++) {
+				// console.log(AbstractBeacon.all_beacons[i].fields.uuid, "===============", beacon_uuid)
+				if (AbstractBeacon.all_beacons[i].fields.uuid == beacon_uuid)
+					return true;
+			}
 		}
 		return false;
 	}
@@ -25,20 +29,10 @@ export default class AbstractBeacon {
 		fetch('http://api.talkingstatues.xyz/beacons')
 		.then((response) => response.json())
 		.then((data) => {
-			console.log("///", data)
-			this.beacons = JSON.parse(data.latest_beacon_list);
-			// console.log('------- this beacons :: ', this.beacons)
+			AbstractBeacon.all_beacons = JSON.parse(data.latest_beacon_list);
+			// console.log("FECTHED", AbstractBeacon.all_beacons);
 		})
 		.catch((error) => console.log(error));
-
-		PushNotification.localNotificationSchedule({
-		  message: "My Notification Message", // (required)
-		  date: new Date(Date.now() + (2 * 1000)), // in 60 secs
-		  actions: '["OK"]',
-		  tag: 'OK',
-		  category: 'OK'
-		});
-
 
 		Beacons.checkTransmissionSupported();
 		Beacons.detectEstimotes();
@@ -51,19 +45,32 @@ export default class AbstractBeacon {
 			'beaconsDidRange',
 			(data) => {
 				// if beacon detected and uuid of beacon belongs to the pre-fetched beacons, then send push notification 
+				console.log(data.beacons);
 				for (let i = 0; i < data.beacons.length; i++) {
-					if (isKnown(data.beacons[i].uuid)) {
-						console.log('beaconsDidRange data: ', data.beacons[0]);
-					// fetch('http://talkingstatues.xyz/api/beaconsDetected')
-					// .then((response) => response.json())
-					// .then((data) => {
-					// 	this.statuesDetected = JSON.parse(data.beaconMatches);
-					// 	for match in matches:
-					// 		send push notificaction with link on the chat
-					// })
-					// .catch((error) => console.log(error));
+					if (AbstractBeacon.isBeaconKnown(data.beacons[i].uuid)) {
+						console.log('beaconsDidRange data: ', data.beacons[i]);
+						if (AbstractBeacon.active_beacons.indexOf(data.beacons[i].uuid) < 0) {
+							AbstractBeacon.active_beacons.push(data.beacons[i].uuid);
+							console.log("NEW BEACONS INSERTED")
+							PushNotification.localNotification({
+								title: "<Statuen>",
+								message: "There is a statue nearby :)", // (required)
+								date: Date.now(),
+								actions: '["Chat !"]',
+								tag: 'OK',
+								category: 'OK'
+							});
+							// fetch('http://talkingstatues.xyz/api/beaconsDetected')
+							// .then((response) => response.json())
+							// .then((data) => {
+							// 	this.statuesDetected = JSON.parse(data.beaconMatches);
+							// 	for match in matches:
+							// 		send push notificaction with link on the chat
+							// })
+							// .catch((error) => console.log(error));
+						}
+					}
 				}
-			}
-		});
+			});
 	}
 }
