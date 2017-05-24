8a9b6edc8ea8f6e57ff3d9743852b8db72ae0f1f
André Eriksen <neskire@outlook.com>
2017-05-24 11:24:12 +0200

    About page add of crucial Anneke icons -.- Target ios 8.0 again for better compatibility. Changed dev team and package name for.

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import {
-	View,
-	Text,
-  	ScrollView,
-  	StyleSheet,
-  	Image,
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
 } from 'react-native'
 
 const styles = StyleSheet.create({
 
   aboutText: {
     fontSize: 16,
-	textAlign: 'center',
+    textAlign: 'center',
     padding:20,
-	paddingTop:0,
+    paddingTop:0,
   },
   aboutHeader: {
     fontSize: 24,
@@ -37,23 +37,24 @@ const styles = StyleSheet.create({
     width:null,
     height:150,
     resizeMode:'contain',
-	flex:.5,
+    flex:.5,
   },
   imageText:{
-  	paddingLeft:40,
-	paddingRight:40,
-	fontSize:10,
+    paddingLeft:40,
+    paddingRight:40,
+    fontSize:10,
   },
   multipleSidebySide:{
-	paddingBottom:20,
+    paddingLeft:20,
+    paddingRight:20,
   }
 })
 
 export default class AboutScreen extends Component {
 
-	render() {
+  render() {
 
-		return (
+    return (
 			<ScrollView style={{flex:1, flexDirection:'column'}}>
 				<View style={{flex:5, backgroundColor:'white'}}>
 					<Text style={styles.aboutHeader}>{`
@@ -74,11 +75,19 @@ Version 0.1
 				<View style={{flex:5}}>
 					<Text style={styles.aboutText}>{`
 This application was made as a project for the Trans-National Creative Exchange (TNCE) in 2017. The aim of TNCE is to be an international platform supporting the emerging creative talent from Europe and China.`}</Text>
-					<View style={styles.multipleSidebySide}>
-						<Image
-								style={styles.sideBySideImage}
-								source={require('./logo_tnce.png')}
-						/>
+					<View style={{paddingLeft:20, paddingRight:20, flex:1, flexDirection:'row'}}>
+						<View style={{flex:1}}>
+							<Image
+									style={styles.sideBySideImage}
+									source={require('./logo_tnce.png')}
+							/>
+						</View>
+						<View style={{flex:3}}>
+							<Image
+									style={styles.sideBySideImage}
+									source={require('./logo_CE.png')}
+							/>
+						</View>
 					</View>
 				</View>
 				<View style={{flex:3, backgroundColor:'white'}}>
@@ -86,12 +95,26 @@ This application was made as a project for the Trans-National Creative Exchange 
 						Statuen is a multi-platform application made by Team EPS intended to make art interactive by using beacons (small wireless locating devices).
 					</Text>
 				</View>
+				<View style={{paddingLeft:20, paddingRight:20, flex:1, flexDirection:'row'}}>
+					<View style={{flex:1}}>
+						<Image
+								style={styles.sideBySideImage}
+								source={require('./logo_eps.png')}
+						/>
+					</View>
+					<View style={{flex:3}}>
+						<Image
+								style={styles.sideBySideImage}
+								source={require('./logo_hhs.png')}
+						/>
+					</View>
+				</View>
 				<View style={{flex:3, backgroundColor:'white'}}>
 					<Text style={styles.aboutHeader}>
 						Thank you
-					  </Text>
+					</Text>
 					<Text style={styles.aboutText}>
-{`Airbnb maps API
+                      {`Airbnb maps API
 Estimote
 Chatterbot
 React Native
@@ -116,7 +139,8 @@ Gifted Chat`}		</Text>
 					/>
 
 				</View>
+
 			</ScrollView>
-		);
-	}
+    );
+  }
 }
